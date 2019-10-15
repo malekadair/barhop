b50c9823a921ecb23124ebb12a4c03ba215d0718
Malek Haj-Hussein <malekadair@gmail.com>
2019-10-15 18:56:58 -0500

    Trying again.

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
-const url = `https://api.yelp.com/v3/businesses/search`
-const api_key = 'S8KmYElllBSl6MnACfDatQhts9YOS0_NwnSRThX8W2AlXKaMAM85JlHDrfEKfCkdmgy8srUjZmd7h3sZKgNoQO0-KKLQRBNtlrDOq2MYnAsktNFIhLi7gtDgUgilXXYx'
-
-const options = {
-	headers: new Headers({
-		'Authorization': 'Bearer %s' % api_key,
-		// 'mode': 'no-cors',
-		'Access-Control-Allow-Origin': true
-	})
-}
 
 const params = {
-			'limit': 50,
-			// 'categories': (bars, All),
-			'radius': 20000,
-			'longitude': '41.936442',
-			'latitude': '-87.641051'}
+	'limit': 50,
+	// 'categories': (bars, All),
+	'radius': 20000,
+	'longitude': '41.936442',
+	'latitude': '-87.641051'}
 
 
-fetch(url, params, options)
-	.then(response => response.json())
-	.then(json => console.log(json))
+const url = `https://api.yelp.com/v3/businesses/search?`+ $.param(params)
+const api_key = 'S8KmYElllBSl6MnACfDatQhts9YOS0_NwnSRThX8W2AlXKaMAM85JlHDrfEKfCkdmgy8srUjZmd7h3sZKgNoQO0-KKLQRBNtlrDOq2MYnAsktNFIhLi7gtDgUgilXXYx'
+
+console.log(url)
+
+const options = {
+	method: 'GET',
+	headers: new Headers({
+		'Authorization': `Bearer ${api_key}`,
+		'Access-Control-Allow-Origin': '*',
+		'Origin':'http://localhost:3000',
+		'Content-Type': 'application/json'
+	}),
+	'mode': 'cors'
+}
+
+
+
+fetch(url, options)
+	.then(response => console.log(response))
+	// .then(json => console.log(json))
+	.catch(error => console.log(error))
+
+
+
+// 	headers.append('Content-Type', 'application/json');
+// headers.append('Accept', 'application/json');
+// headers.append('Authorization', 'Basic ' + base64.encode(username + ":" +  password));
+// headers.append('Origin','http://localhost:3000');
+
+// fetch(sign_in, {
+//     mode: 'cors',
+//     credentials: 'include',
+//     method: 'POST',
+//     headers: headers
+// })
