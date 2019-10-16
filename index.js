bb4d9d22739856678ba84ad18fb0dd0f3b3bc01d
Malek Haj-Hussein <malekadair@gmail.com>
2019-10-15 20:50:52 -0500

    Got working Fetch Requestgit add .git add .

--- index.js
+++ index.js
@@ -1,45 +1,31 @@
 
-const params = {
-	'limit': 50,
-	// 'categories': (bars, All),
-	'radius': 20000,
-	'longitude': '41.936442',
-	'latitude': '-87.641051'}
-
-
-const url = `https://api.yelp.com/v3/businesses/search?`+ $.param(params)
 const api_key = 'S8KmYElllBSl6MnACfDatQhts9YOS0_NwnSRThX8W2AlXKaMAM85JlHDrfEKfCkdmgy8srUjZmd7h3sZKgNoQO0-KKLQRBNtlrDOq2MYnAsktNFIhLi7gtDgUgilXXYx'
 
-console.log(url)
+let url = new URL('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search')
 
 const options = {
 	method: 'GET',
 	headers: new Headers({
 		'Authorization': `Bearer ${api_key}`,
-		'Access-Control-Allow-Origin': '*',
-		'Origin':'http://localhost:3000',
 		'Content-Type': 'application/json'
-	}),
-	'mode': 'cors'
+	})
+}
+
+const params = {
+	'limit': 50,
+	// 'categories': (bars, All),
+	'radius': 20000,
+	'longitude': '41.936442',
+	'latitude': '-87.641051'
 }
 
 
+Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
 
-fetch(url, options)
-	.then(response => console.log(response))
-	// .then(json => console.log(json))
-	.catch(error => console.log(error))
-
-
-
-// 	headers.append('Content-Type', 'application/json');
-// headers.append('Accept', 'application/json');
-// headers.append('Authorization', 'Basic ' + base64.encode(username + ":" +  password));
-// headers.append('Origin','http://localhost:3000');
-
-// fetch(sign_in, {
-//     mode: 'cors',
-//     credentials: 'include',
-//     method: 'POST',
-//     headers: headers
-// })
+$(document).ready(function(){
+	$("#button").click(function(){
+		fetch(url, options)
+			.then(res => res.json())
+			.then(data => $("#results").text(data.businesses[0].name))
+	});
+});
